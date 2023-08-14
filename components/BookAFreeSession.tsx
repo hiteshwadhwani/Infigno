05b28da0ce51ca919a8ee6a60a5d8efc5a6c18a2
Hitesh <hitesh@localhost.localdomain>
2023-08-14 17:35:17 +0530

    updated

--- components/BookAFreeSession.tsx
+++ components/BookAFreeSession.tsx
@@ -5,12 +5,18 @@ import { Button } from "./ui/button";
 const font = Quicksand({ subsets: ["latin"] });
 
 const BookAFreeSession = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth', // You can use 'auto' for instant scrolling
+    });
+  };
   return (
     <div className="md:hidden p-[20px] grid grid-cols-2 gap-x-[20px] fixed bottom-4 bg-white customShadow mx-4 rounded-[10px] z-30">
       <div className={twMerge("text-[16px] text-[#0047AB]", font.className)}>
         Book a <span className="font-bold">Free Demo Session</span>
       </div>
-      <Button className="font-semibold text-white py-[12px] px-[20px] rounded-full bg-[#0047AB] hover:bg-[#0047AB] hover:opacity-80 cursor-pointer">Register NOW</Button>
+      <Button onClick={scrollToTop} className="font-semibold text-white py-[12px] px-[20px] rounded-full bg-[#0047AB] hover:bg-[#0047AB] hover:opacity-80 cursor-pointer">Register NOW</Button>
     </div>
   );
 };
